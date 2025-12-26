8a9c087568dcffc0d965206b5829cc83f3fb3763
benjamin <benjamin.gammage@gmail.com>
2025-12-26 23:46:10 +0000

    started chapter 3, loops

---
+++ if-else_flow-control_2/dishonestcapacity.py
@@ -0,0 +1,19 @@
+print('Enter TB or GB for the advertised unit:')
+unit = input('>')
+
+#calculate the amount that the advertised capacity lies:
+if unit == 'TB' or unit == 'tb':
+    discrepency = 1000000000000 / 1099511627776
+elif unit == 'GB' or unit == 'gb':
+    discrepency = 1000000000 / 1073741824
+    
+print("Enter the advertised capacity")
+advertised_capacity = input('>')
+advertised_capacity = float(advertised_capacity)
+
+#calculate the real capacity, round it to the nearest hundredths,
+#convert it to a string so it can be concatenated:
+real_capacity = str(round(advertised_capacity * discrepency, 2))
+
+print('the actual capacity is' + real_capacity + '' + unit)
+
